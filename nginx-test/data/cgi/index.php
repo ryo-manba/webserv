1ee74c28ffcd104fab22c05e313ba226adc4148d
Ryomanba <datemanba3@gmail.com>
2022-06-02 22:46:01 +0900

    add: nginx-conf

---
+++ nginx-test/data/cgi/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "Hi, I'm a PHP script!";
+            ?>
+        </h1>
+    </body>
+</html>
---
+++ nginx-test/data/cgi/sample.php
@@ -0,0 +1,16 @@
+<?php
+    $name = "42 Tokyo";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Hello World</title>
+</head>
+<body>
+<?php
+    echo "現在時刻 : " .date("Y-m-d H:i:s"). "<br>";
+    echo $name ."は、フランス発のエンジニア養成機関です。"
+?>
+</body>
+</html>
